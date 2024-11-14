3a9173f7bf2de393f7c522e880d3451a011273fe
Jeremy Holland <jpholland05@gmail.com>
2024-11-13 19:31:43 -0500

    productExceptSelf

--- arrays/productExceptSelf.go
+++ arrays/productExceptSelf.go
@@ -1,6 +1,22 @@
 package arrays
 
 func ProductExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	for i := range res {
+		res[i] = 1
+	}
 
-	return []int{}
+	pre := 1
+	for i := 0; i < len(nums); i++ {
+		res[i] = pre
+		pre *= nums[i]
+	}
+
+	post := 1
+	for i := len(nums); i >= 0; i-- {
+		res[i] *= post
+		post *= nums[i]
+	}
+
+	return res
 }
